226faff7bb751b5838b6717f629323fef6b6099f
Kirilllka1993 <kirilllka1993@gmail.com>
2023-07-24 20:02:40 +0300

    Новое изменение

---
+++ src/by/pvt/academy/Hello.java
@@ -0,0 +1,7 @@
+package by.pvt.academy;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git. It is also difficult");
+    }
+}
--- src/by/pvt/academy/Main.java
+++ src/by/pvt/academy/Main.java
@@ -3,6 +3,6 @@ package by.pvt.academy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Git");
     }
 }
